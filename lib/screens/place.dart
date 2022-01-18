3d8cb526596904ffcb84177a7849de6c001f1dbe
HuynhXuanThinh_CDTH19PMC <78005011+HuynhXuanThinh-CDTH19PMC@users.noreply.github.com>
2022-01-18 16:17:11 +0700

    them load dia danh

--- lib/screens/place.dart
+++ lib/screens/place.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:map_launcher/map_launcher.dart';
+import 'api.dart';
 
 class MyGridScreen extends StatefulWidget {
   MyGridScreen({Key? key}) : super(key: key);
@@ -9,23 +11,74 @@ class MyGridScreen extends StatefulWidget {
 }
 
 class _MyGridScreenState extends State<MyGridScreen> {
+  Iterable s = [];
+  bool isUpdate = true;
+  Future<void> local(double a, double b) async {
+    final availableMap = await MapLauncher.installedMaps;
+    await availableMap.first.showMarker(
+      coords: Coords(a, b),
+      title: "Local",
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    API(url: "http://10.0.2.2/dictionary/api/lay_dia_danh.php")
+        .getDataString()
+        .then((value) {
+      s = json.decode(value);
+      isUpdate = false;
+      setState(() {});
+    });
     return Container(
-      padding: EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              buildCard(),
-              buildCard(),
-              buildCard(),
-              buildCard(),
-              buildCard(),
-              buildCard(),
-              buildCard(),
-            ],
-          )),
+      padding: const EdgeInsets.only(top: 20),
+      child: SizedBox(
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: s.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 4.0,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(s.elementAt(index)["name"].toString()),
+                      trailing: Icon(Icons.favorite_outline),
+                    ),
+                    Container(
+                      height: 200.0,
+                      child: Ink.image(
+                        image: AssetImage('assets/images/beach.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      alignment: Alignment.centerLeft,
+                      child: Text(s.elementAt(index)["description"].toString()),
+                    ),
+                    ButtonBar(
+                      children: [
+                        TextButton(
+                          child: const Text('CONTACT AGENT'),
+                          onPressed: () {
+                            local(double.parse(s.elementAt(index)["location1"]),
+                                double.parse(s.elementAt(index)["location2"]));
+                          },
+                        ),
+                        TextButton(
+                          child: const Text('LEARN MORE'),
+                          onPressed: () {/* ... */},
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
